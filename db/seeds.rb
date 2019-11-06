2439ba131de1aee936e90be0b5836092bae9cba1
Don Petersen <don@donpetersen.net>
2019-11-06 14:51:33 -0800

    Database, seeds, runnable in container

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+PERSON_COUNT = 100
+PERSON_COUNT.times do
+  Person.create!(name: Faker::Name.name)
+end
+Rails.logger.info("Created #{PERSON_COUNT} People")
